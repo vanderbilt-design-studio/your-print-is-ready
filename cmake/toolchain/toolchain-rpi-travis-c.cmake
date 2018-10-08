26eac51d95b14d3420179a0758ea81cdf45b945d
Sameer <11097096+sameer@users.noreply.github.com>
2018-10-07 23:18:07 -0500

    Update toolchains to handle travis

---
+++ cmake/toolchain/toolchain-rpi-travis-c.cmake
@@ -0,0 +1,21 @@
+# Define our host system
+SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_SYSTEM_VERSION 1)
+
+# Define the cross compiler locations
+SET(CMAKE_C_COMPILER  $ENV{TRAVIS_BUILD_DIR}/your-print-is-ready/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER $ENV{TRAVIS_BUILD_DIR}/your-print-is-ready/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc)
+# Define the sysroot path for the RaspberryPi distribution in our tools folder 
+SET(CMAKE_FIND_ROOT_PATH $ENV{TRAVIS_BUILD_DIR}/your-print-is-ready/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot/)
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --sysroot=${CMAKE_FIND_ROOT_PATH}")
+SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} --sysroot=${CMAKE_FIND_ROOT_PATH}")
+SET(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} --sysroot=${CMAKE_FIND_ROOT_PATH}")
+
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+add_definitions(-Wall -std=c11)
