ee9aca47685bd04930bee0a08c023982fe5c894d
Sameer <11097096+sameer@users.noreply.github.com>
2018-10-11 12:07:01 -0500

    Do a barrel roll

--- poller-pi.py
+++ poller-pi.py
@@ -8,6 +8,7 @@ import socket
 from ultimaker import Printer
 from config import server_uri, ultimaker_application_name, ultimaker_user_name
 from uuid import UUID
+import json
 
 printers: Dict[str, Printer] = {}
 
@@ -32,10 +33,21 @@ browser = ServiceBrowser(zeroconf, "_printer._tcp.local.", listener)
 async def send_printer_status():
     async with websockets.connect(server_uri) as websocket:
         while True:
-            await websocket.send("Hello world!")
+            printer: Printer
+            for printer in list(printers.values()):
+                try:
+                    printer_info: Dict[str, str] = {
+                        'guid': printer.guid,
+                        'printer_status': printer.get_printer_status(),
+                        'print_job_state': printer.get_print_job_state()
+                    }
+                except Exception as e:
+                    print(
+                        f'Exception getting info for printer {printer.guid}, it may no longer exist: {e}')
+                    continue
+                await websocket.send(json.dumps(printer_info))
 
 try:
-    # asyncio.get_event_loop().run_until_complete(send_printer_status())
-    input("Press enter to exit...\n\n")
+    asyncio.get_event_loop().run_until_complete(send_printer_status())
 finally:
     zeroconf.close()
