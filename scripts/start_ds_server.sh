81590d07a89fc5d12c34422aae6aab7886dc8448
Sameer <11097096+sameer@users.noreply.github.com>
2018-10-15 12:26:28 -0500

    Start the server properly

--- scripts/start_ds_server.sh
+++ scripts/start_ds_server.sh
@@ -1,3 +1,1 @@
-cd /opt/your-print-is-ready/
-source .env/bin/activate
-python3 ds-server.py > /dev/null 2>/dev/null < /dev/null &
+sudo su -c 'cd /opt/your-print-is-ready/ && source .env/bin/activate && nohup python3 ds-server.py > /dev/null 2>/dev/null < /dev/null &'
