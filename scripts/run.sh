550f3a042815460cd79ae4d0a77395dfb4d0ee95
Sameer <11097096+sameer@users.noreply.github.com>
2018-10-16 09:32:07 -0500

    Use screen

---
+++ scripts/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /opt/your-print-is-ready/
+source .env/bin/activate
+python3 ds-server.py
--- scripts/start_ds_server.sh
+++ scripts/start_ds_server.sh
@@ -1,1 +1,1 @@
-sudo su -c 'cd /opt/your-print-is-ready/ && source .env/bin/activate && nohup python3 ds-server.py' > /dev/null 2>/dev/null < /dev/null &
+sudo su -c 'screen -dm -S your-print-is-ready /opt/your-print-is-ready/scripts/run.sh' > /dev/null 2>/dev/null < /dev/null &
