7d4aa2fb4ae2eefed1e63379a558f45394443e4b
Sameer <11097096+sameer@users.noreply.github.com>
2018-10-16 08:57:16 -0500

    Fix AWS CodeDeploy hang, kill -9 if kill fails b/c don't care about log or message integrity tbh

--- scripts/start_ds_server.sh
+++ scripts/start_ds_server.sh
@@ -1,1 +1,1 @@
-sudo su -c 'cd /opt/your-print-is-ready/ && source .env/bin/activate && nohup python3 ds-server.py > /dev/null 2>/dev/null < /dev/null &'
+sudo su -c 'cd /opt/your-print-is-ready/ && source .env/bin/activate && nohup python3 ds-server.py' > /dev/null 2>/dev/null < /dev/null &
--- scripts/stop_ds_server.sh
+++ scripts/stop_ds_server.sh
@@ -1,2 +1,3 @@
 kill `pgrep -f ds-server.py`
+kill -9 `pgrep -f ds-server.py`
 rm -rf /opt/your-print-is-ready/
